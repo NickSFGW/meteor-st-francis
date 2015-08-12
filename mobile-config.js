2681c8929c0add961c77d591233eef5bcd8cf522
Nicolas Vigier <nvigier46@gmail.com>
2015-08-12 15:48:14 -0700

    added Android icons + UI tweaks

---
+++ mobile-config.js
@@ -0,0 +1,20 @@
+App.info({
+	id: "come.sfgoodwill.stfrancis",
+	name: "Donation App",
+	description: "Donation app for SF Goodwill",
+	author: "Nicolas Vigier",
+	website: "http://sfgoodwill.org"
+});
+
+App.accessRule("https://fonts.googleapis.com");
+App.accessRule("https://fonts.gstatic.com");
+App.accessRule("http://104.197.87.34");
+
+App.icons({
+	'android_ldpi' : 'public/img/gw-icon-36.png',
+	'android_mdpi' : 'public/img/gw-icon-48.png',
+	'android_hdpi' : 'public/img/gw-icon-72.png',
+	'android_xhdpi' : 'public/img/gw-icon-96.png'
+	//'android_xxhdpi' : 'public/img/gw-icon-144.png',
+	//'android_xxxhdpi' : 'public/img/gw-icon-192.png'
+});
